2cd636c6144fbe4b5386bb0770d7da370978c66f
Dima-Shvets <81581083+Dima-Shvets@users.noreply.github.com>
2021-10-07 20:19:16 +0300

    додає файли з виконаними завданнями

--- js/script.js
+++ js/script.js
@@ -1,12 +1,35 @@
-const colors = [
-  '#FFFFFF',
-  '#2196F3',
-  '#4CAF50',
-  '#FF9800',
-  '#009688',
-  '#795548',
-];
+import colors from './data.js'
+import refs from './refs.js'
+
+const INTERVAL = 1000;
+let intervalId = null;
 
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+const setBackgroundColor = () => {
+  refs.body.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length - 1)]
+}
+
+const onStartBtnPress = () => {
+  intervalId = setInterval(() => {
+  setBackgroundColor()
+},
+    INTERVAL)
+  
+  refs.startBtn.disabled = true;
+}
+
+const onStopBtnPress = () => {
+  clearInterval(intervalId);
+
+  refs.startBtn.disabled = false;
+}
+
+refs.startBtn.addEventListener('click', onStartBtnPress);
+refs.stopBtn.addEventListener('click', onStopBtnPress);
+
+
+
+
